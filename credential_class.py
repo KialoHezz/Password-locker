9c0cc635999d97e566db4ef6d5a4394df3f5d1a5
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-04-23 15:19:16 +0300

    create the class(user and credential) and instanciated the classes and have properties in the constructor such as username and password

---
+++ credential_class.py
@@ -0,0 +1,13 @@
+'''
+Created credential class
+'''
+class credential:
+    '''
+    created new instance of a class and have passed in properties of the object
+    '''
+    def __init__(self,username,password):
+        '''
+        instance variable where they take up - username and password
+        '''
+        self.username = username
+        self.password = password
---
+++ user_class.py
@@ -0,0 +1,13 @@
+'''
+Created user class
+'''
+class user:
+    '''
+    created new instance of a class and have passed in properties of the object
+    '''
+    def __init__(self,username,password):
+        '''
+        instance variable where they take up - username and password
+        '''
+        self.username = username
+        self.password = password
