71dcb014d82216ed72d7b0f4330863c12b296c0e
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-04-24 23:54:50 +0300

    Control the users flows :To create account,login,exit and including the function where are need

--- main.py
+++ main.py
@@ -44,3 +44,65 @@ def display_credentials(userName):
 	Function to display credentials saved by a user
 	'''
 	return credential.display_credentials(userName)
+
+
+def main ():
+    print('Please enter your name:')
+    use_name = input()
+
+    print(f'Hello! {use_name} Welcome to password locker')
+
+    while True:
+        print('Use these codes to navigate: \n CA-Create an Account \n LI-Log In \n EX -Exit')
+        short_code = input('Enter your choice:').lower()
+
+        if short_code == "CA":
+            print('print  to create new account: + ' '*10')
+            username = input("Enter your username + ' '*10")
+            password = int(input("Your password: + ' '*10 "))
+            save_user(create_user(username,password))
+
+            print(f'New account for : {username}  created successfully ')
+        
+        elif short_code == "LI":
+            print("Loging into existing account to our system:")
+
+            username = input('Enter your first name - ')
+            password = int(input('Enter your password - '))
+
+            user_exists = verify_user(username,password)
+
+            if user_exists == username:
+                print(f'Welcome {username}. Please choose an option to continue.')
+
+                while True:
+                    print('Use these codes to navigate: \n CA-Create an Account \n  CC- copy \n EX -Exit')
+                    short_code = input('Enter your choice:').lower()
+
+                    if short_code == 'EX':
+                        print(f'Goodbye {username}')
+					
+                    elif short_code  == 'CC':
+                        print('Enter your credential details:')
+
+
+                        userName = input('enter the username')
+                        siteName = input('Enter the site name- ')
+                        accountName = input('Enter your account  name - ')
+
+                        save_credential(create_credential(userName,siteName,accountName,password))
+                        print(f'Credential Created: Site Name: {siteName} - Account Name: {accountName} - Password: {password}')
+                    
+
+                    else:
+                        print('Oop! Wrong details entered . Try again or Creae another account' )
+
+                else:
+                    print("-"*60)
+                    print(' ')
+                    print('Oops! Wrong option entered. Try again.')
+
+
+
+if __name__ == '__main__':
+	main()
